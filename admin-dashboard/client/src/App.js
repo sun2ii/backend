0cd658eda2356fffd54d6155229f2ef08b9608b9
sun2ii <sun2ii.mac>
2023-02-11 19:24:56 -0600

    added dark/light mode buttons

--- admin-dashboard/client/src/App.js
+++ admin-dashboard/client/src/App.js
@@ -1,16 +1,27 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "theme";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "scenes/dashboard";
+import Layout from "scenes/layout";
 
 function App() {
-  const mode = useSelector(state => state.global.mode);
+  const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return <div className="App">
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Navigate to="/dashboard" replace />}/>
+            <Route path="/dashboard" element={<Dashboard />}/>
+          </Route>
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   </div>
 }
 
